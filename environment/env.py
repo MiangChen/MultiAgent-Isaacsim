785bb5aac37774df696477a3c68d3b3dd7d1af23
MiangChen <2594416505@qq.com>
2025-08-26 10:55:19 +0800

    update

--- environment/env.py
+++ environment/env.py
@@ -1,37 +1,8 @@
 import gymnasium as gym
 
-from files.assets_scripts_linux import PATH_PROJECT, PATH_ISAACSIM_ASSETS
 from map.map_grid_map import GridMap
 from robot.swarm_manager import SwarmManager
 from scene.scene_manager import SceneManager
-
-import importlib.util
-# try:
-#     current_dir = os.path.dirname(os.path.abspath(__file__))
-# except NameError:
-#     current_dir = os.getcwd()
-# relative_path_to_module = "../mcp_extension/isaacsim.mcp_extension/isaacsim_mcp_extension/extension.py"
-# absolute_path = os.path.join(current_dir, relative_path_to_module)
-
-# module_name = "scene_manager"
-
-# # 创建一个模块规范 (Module Spec)
-# spec = importlib.util.spec_from_file_location(module_name, absolute_path)
-#
-# # 根据规范创建并执行模块加载
-# if spec and spec.loader:
-#     my_extension_module = importlib.util.module_from_spec(spec)
-#
-#     # 将模块添加到 sys.modules，这样其他地方也可以 import my_mcp_extension
-#     # sys.modules[module_name] = my_extension_module
-#
-#     spec.loader.exec_module(my_extension_module)
-#
-# spec = importlib.util.spec_from_file_location(module_name, absolute_path)
-# my_extension_module = importlib.util.module_from_spec(spec)
-# spec.loader.exec_module(my_extension_module)
-#
-# scene_manager = my_extension_module.MCPExtension()
 
 
 class Env(gym.Env):
