010ebae67af9cc115a673938fea9b7d3a1163903
MiangChen <2594416505@qq.com>
2025-08-28 20:07:47 +0800

    update

--- trash/code_standards_formatter/core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Core components for the code standards formatter.
-
-This package contains the main formatting logic and processing components.
-"""
-
-from .file_scanner import FileScanner
-
-__all__ = ['FileScanner']
--- trash/code_standards_formatter/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Utility functions and helpers for the code standards formatter.
-
-This package contains common utilities used across the formatting system.
-"""
