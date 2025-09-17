693e17162d831f68eddb3d980f44a4cc58697762
MiangChen <2594416505@qq.com>
2025-09-17 20:18:28 +0800

    update

--- gsi_msgs/install/scene_msgs/share/scene_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(scene_msgs_IDL_FILES "msg/PrimTransform.idl;msg/SceneModifications.idl")
-set(scene_msgs_INTERFACE_FILES "msg/PrimTransform.msg;msg/SceneModifications.msg")
